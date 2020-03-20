2cca52da340ebb40af4b73ea11cc7f2e1340aa54
Dhawal Singh Panwar <dsp9107@outlook.com>
2020-03-20 15:07:52 +0530

    Update PyAttendance.py

--- PyAttendance.py
+++ PyAttendance.py
@@ -43,15 +43,24 @@ for sub in att['attendance'] :
     print(f"{sub['Title']} : {str(p)}%\n")
 
     if p >= 90 :
+        print("- Can Leave " + str(absents(a,d,p,60)) + " Lectures And Still Remain Above 60%")
         print("- Can Leave " + str(absents(a,d,p,75)) + " Lectures And Still Remain Above 75%")
         print("- Can Leave " + str(absents(a,d,p,90)) + " Lectures And Still Remain Above 90%")
 
     elif p >= 75 :
+        print("- Can Leave " + str(absents(a,d,p,70)) + " Lectures And Still Remain Above 70%")
         print("- Can Leave " + str(absents(a,d,p,75)) + " Lectures And Still Remain Above 75%")
         print("- Attend " + str(presents(a,d,p,90)) + " Lectures To Attain 90%")
 
-    else :
+    elif p >= 60 :
+        print("- Can Leave " + str(absents(a,d,p,65)) + " Lectures And Still Remain Above 65%")
         print("- Attend " + str(presents(a,d,p,75)) + " Lectures To Attain 75%")
         print("- Attend " + str(presents(a,d,p,90)) + " Lectures To Attain 90%")
+
+    else :
+        print("- Attend " + str(presents(a,d,p,60)) + " Lectures To Attain 60%")
+        print("- Attend " + str(presents(a,d,p,75)) + " Lectures To Attain 75%")
+        print("- Attend " + str(presents(a,d,p,90)) + " Lectures To Attain 90%")
+
     print()
 
